00f9e2e69093209cfc86eb3ebe75634fc8d4165f
joaquinsalinas06 <103973127+joaquinsalinas06@users.noreply.github.com>
2024-09-21 12:36:38 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,7 +1,7 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "jss-output-s3"
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
